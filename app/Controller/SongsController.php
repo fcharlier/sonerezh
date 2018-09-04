8ba211bec7cce3319df14c72c20d301e0770db66
Francois Charlier <naz@ploup.net>
2018-09-04 13:08:44 +0000

    display randomly-selected albums on Albums page #298

--- app/Controller/SongsController.php
+++ app/Controller/SongsController.php
@@ -330,9 +330,16 @@ class SongsController extends AppController {
         ));
 
         $latests = array();
+        $rands = array();
         // Is this the first page requested?
         $page = isset($this->request->params['named']['page']) ? $this->request->params['named']['page'] : 1;
         $db = $this->Song->getDataSource();
+
+        if ($db->config['datasource'] == 'Database/Mysql') {
+            $random_order_by_method = 'RAND()';
+        } else {
+            $random_order_by_method = 'RANDOM()';
+        }
 
         $this->Song->virtualFields['cover'] = 'MIN(Song.cover)';
 
@@ -341,6 +348,12 @@ class SongsController extends AppController {
                 'fields' => array('Song.band', 'Song.album', 'cover'),
                 'group' => array('Song.album', 'Song.band'),
                 'order' => 'MAX(Song.created) DESC',
+                'limit' => LATEST_NUM_ROWS * 6
+            ));
+            $rands = $this->Song->find('all', array(
+                'fields' => array('Song.band', 'Song.album', 'cover'),
+                'group' => array('Song.album', 'Song.band'),
+                'order' => $random_order_by_method,
                 'limit' => LATEST_NUM_ROWS * 6
             ));
         }
@@ -364,11 +377,15 @@ class SongsController extends AppController {
             $latest['Song']['cover'] = empty($latest['Song']['cover']) ? "no-cover.png" : THUMBNAILS_DIR.'/'.$latest['Song']['cover'];
         }
 
+        foreach ($rands as &$rand) {
+            $rand['Song']['cover'] = empty($rand['Song']['cover']) ? "no-cover.png" : THUMBNAILS_DIR.'/'.$rand['Song']['cover'];
+        }
+
         if (empty($songs)) {
             $this->Flash->info(__('Oops! The database is empty...'));
         }
 
-        $this->set(compact('songs', 'playlists', 'latests'));
+        $this->set(compact('songs', 'playlists', 'latests', 'rands'));
     }
 
     /**
